ecbaf7b92ba9d29fd579b7f36c7523b5c760f9b1
moakilodash <torkimohammad4@gmail.com>
2025-07-05 10:04:25 +0200

    chore: update examples and package configuration

--- typescript/examples/basic-usage.ts
+++ typescript/examples/basic-usage.ts
@@ -3,10 +3,10 @@ import { KaleidoClient } from "../src/client"
 async function main() {
     const client = new KaleidoClient({
         baseUrl: 'http://localhost:8000/api/v1',
-        nodeUrl: 'http://localhost:3001/api/v1'
+        nodeUrl: 'http://localhost:3001/'
     })
 
-    const nodeInfo = await client.getInfo()
+    const nodeInfo = await client.getNodeInfo()
     console.log(nodeInfo)
 }
 
